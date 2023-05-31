61662f4ea31e06a8a8c22cb79417f873f05c0e49
bobochang <m13726750698_2@163.com>
2023-05-31 17:27:26 +0800

    feat:  去除冗余代码

--- bobochangAPI-interface/src/main/java/com/bobochang/apiinterface/controller/NameController.java
+++ bobochangAPI-interface/src/main/java/com/bobochang/apiinterface/controller/NameController.java
@@ -28,8 +28,9 @@ public class NameController {
 
     @PostMapping("/json")
     public String getUserNameByPost(@RequestBody User user, HttpServletRequest request) {
-        String accessKey = request.getHeader("accessKey");
-        /*
+        // 下述操作都已经在网关拦截器中实现
+        /*String accessKey = request.getHeader("accessKey");
+        *//*
          String nonce = request.getHeader("nonce");
          String timestamp = request.getHeader("timestamp");
         if (Long.parseLong(nonce) > 10000) {
@@ -38,7 +39,7 @@ public class NameController {
         // 时间和当前时间不能超过 5 分钟
         if (System.currentTimeMillis() - Long.parseLong(timestamp) > TimeUnit.MINUTES.toMillis(5)) {
             throw new RuntimeException("无权限");
-        }*/
+        }*//*
         String sign = request.getHeader("sign");
         String body = request.getHeader("body");
         // 查询用户是否有 accessKey
@@ -50,7 +51,7 @@ public class NameController {
         String serverSign = SignUtils.genSign(body, "337377b1423e5c473827b98eeae4eda2");
         if (!sign.equals(serverSign)) {
             throw new RuntimeException("无权限");
-        }
+        }*/
         return "【POST请求方式 - JSON传参】 你的名字是：" + user.getUsername();
     }
 }
