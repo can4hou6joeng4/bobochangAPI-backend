d207802cae3b1ee87723254757db5097dfeb89e0
bobochang <m13726750698_2@163.com>
2023-05-31 10:16:03 +0800

    feat:  抽象出启动类 bean

--- bobochangAPI-gateway/src/main/java/com/bobochang/apigateway/GatewayApplication.java
+++ bobochangAPI-gateway/src/main/java/com/bobochang/apigateway/GatewayApplication.java
@@ -1,12 +1,8 @@
 package com.bobochang.apigateway;
 
-import com.bobochang.apigateway.provider.DemoService;
-import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.stereotype.Service;
 
 /**
  * 2023/5/30 - 10:35
