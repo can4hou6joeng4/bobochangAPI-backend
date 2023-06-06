c810c093c73cc955bcb2e973c755f4d4b5a94b5e
bobochang <m13726750698_2@163.com>
2023-06-06 16:15:41 +0800

    feat: 完善网关模块启动类

--- bobochangAPI-gateway/src/main/java/com/bobochang/apigateway/GatewayApplication.java
+++ bobochangAPI-gateway/src/main/java/com/bobochang/apigateway/GatewayApplication.java
@@ -4,6 +4,8 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * 2023/5/30 - 10:35
@@ -11,7 +13,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author bobochang
  * @description
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(
+        exclude = {DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 @EnableDubbo
 public class GatewayApplication {
 
