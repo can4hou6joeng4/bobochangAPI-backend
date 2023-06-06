03199fb3a84476d8b939443fb56f573fbd14682e
bobochang <m13726750698_2@163.com>
2023-06-07 00:43:39 +0800

    feat: 修改文件说明

--- bobochangAPI-backend/src/main/java/com/bobochang/apiplatform/mapper/UserInterfaceInfoMapper.java
+++ bobochangAPI-backend/src/main/java/com/bobochang/apiplatform/mapper/UserInterfaceInfoMapper.java
@@ -7,7 +7,7 @@ import com.bobochang.apicommon.model.entity.UserInterfaceInfo;
 * @author bobochang
 * @description 针对表【user_interface_info(用户调用接口关系)】的数据库操作Mapper
 * @createDate 2023-05-26 13:48:25
-* @Entity com.bobochang.apiplatform.model.entity.UserInterfaceInfo
+* @Entity com.bobochang.apicommon.model.entity.UserInterfaceInfo
 */
 public interface UserInterfaceInfoMapper extends BaseMapper<UserInterfaceInfo> {
 
--- bobochangAPI-backend/src/main/java/com/bobochang/apiplatform/mapper/UserMapper.java
+++ bobochangAPI-backend/src/main/java/com/bobochang/apiplatform/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import com.bobochang.apicommon.model.entity.User;
 * @author m1372
 * @description 针对表【user(用户)】的数据库操作Mapper
 * @createDate 2023-05-27 22:17:31
-* @Entity com.bobochang.apiplatform.model.entity.User
+* @Entity com.bobochang.apicommon.model.entity.User
 */
 public interface UserMapper extends BaseMapper<User> {
 
