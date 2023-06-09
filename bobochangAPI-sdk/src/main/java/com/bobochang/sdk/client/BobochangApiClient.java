1d417483e7855c3f86935ff383b0c04a74fd18fb
bobochang <m13726750698_2@163.com>
2023-06-09 09:03:41 +0800

    feat: 完善 sdk 客户端

--- bobochangAPI-sdk/src/main/java/com/bobochang/sdk/client/BobochangApiClient.java
+++ bobochangAPI-sdk/src/main/java/com/bobochang/sdk/client/BobochangApiClient.java
@@ -1,10 +1,18 @@
 package com.bobochang.sdk.client;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
+import com.bobochang.sdk.model.AvatarParams;
 import com.bobochang.sdk.model.User;
+import com.bobochang.sdk.uils.HttpResponseDataUtils;
+import com.bobochang.sdk.uils.Params2JsonUtils;
 import com.bobochang.sdk.uils.SignUtils;
+import com.google.gson.JsonElement;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +22,7 @@ import java.util.Map;
  */
 public class BobochangApiClient {
 
-    private final String GATEWAY_HOST = "http://localhost:9000";
+    private String GATEWAY_HOST = "http://175.178.234.102:9000";
     /**
      * 用户授权名
      */
@@ -52,7 +60,7 @@ public class BobochangApiClient {
         String json = JSONUtil.toJsonStr(user);
 
         HttpResponse response = HttpRequest
-                .post(GATEWAY_HOST + "/api/name/json")
+                .post(GATEWAY_HOST + "/api/invoke/name/json")
                 .addHeaders(getHeaderMap(json))
                 .body(json)
                 .execute();
@@ -60,18 +68,38 @@ public class BobochangApiClient {
 
     }
 
-    /**
-     * 添加请求头
-     *
-     * @return
-     */
     private Map<String, String> getHeaderMap(String body) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("accessKey", accessKey);// 授权名
-        // map.put("nonce", RandomUtil.randomNumbers(4));// 随机数
-        map.put("body", body);// 用户传递的参数
-        map.put("sign", SignUtils.genSign(body, secretKey));// 生成的签名
+        Map<String, String> hasMap = new HashMap<>();
+        String nonce = RandomUtil.randomNumbers(4);
+        String currentTime = String.valueOf(System.currentTimeMillis() / 1000);
+        hasMap.put("accessKey", accessKey);
+        hasMap.put("nonce", nonce);
+        hasMap.put("body", body);
+        hasMap.put("timestamp", currentTime);
+        hasMap.put("sign", SignUtils.genSign(body, secretKey));
+        return hasMap;
+    }
 
-        return map;
+    /**
+     * 随机输出一句冷笑话
+     * charset 返回编码类型[gbk|utf-8]默认utf-8
+     * encode 返回格式类型[text|js|json]默认text
+     *
+     * @return 状态码和响应值
+     */
+    public ImmutablePair<Integer, String> randomMessage(String params) {
+        String charset, encode;
+        HttpRequest request = HttpRequest.get(GATEWAY_HOST + "/api/invoke/randomMessage");
+        if (StringUtils.isNotBlank(params)) {
+            JsonElement jsonParams = Params2JsonUtils.getJsonParams(params);
+            charset = jsonParams.getAsJsonObject().get("charset").getAsString();
+            encode = jsonParams.getAsJsonObject().get("encode").getAsString();
+            // 不传参时使用默认参数
+            request = StringUtils.isNotBlank(charset) ? request.form("charset", charset) : request.form("charset", "utf-8");
+            request = StringUtils.isNotBlank(encode) ? request.form("encode", encode) : request.form("encode", "text");
+        }
+
+        HttpResponse response = request.addHeaders(getHeaderMap(params)).execute();
+        return HttpResponseDataUtils.resData(response);
     }
 }
